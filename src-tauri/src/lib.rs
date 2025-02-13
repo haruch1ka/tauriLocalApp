5e91701aae808413bb488177b989e782f4e9563c
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-02-13 14:13:09 +0900

    @tauri-apps/plugin-fsを追加し、ファイル選択機能を実装

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,22 +4,25 @@ use std::string::String;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-    println!("Hello from Rust!");    
+    println!("Hello from Rust!");
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-
 
 // execute powershell script
 #[tauri::command]
 fn run_script(script_path: &str) -> Result<String, String> {
     let output = Command::new("powershell")
-        .arg("-File")
-        .arg(script_path)
+        .arg("-Command")
+        .arg(format!(
+            //制作したcomobjectを使ってillustratorを起動し、指定したスクリプトを実行する。main()を追記して、スクリプト内の関数を実行する。
+            "$appRef = New-Object -ComObject Illustrator.Application; $appRef.DoJavaScript((Get-Content -Path '{}' -Raw)+\";main();\");",
+            script_path
+        ))
         .output()
         .map_err(|e| format!("failed to execute process: {}", e))?;
 
-    let stdout = String::from_utf8(output.stdout).map_err(|e| format!("failed to parse stdout: {}", e))?;
-    let stderr = String::from_utf8(output.stderr).map_err(|e| format!("failed to parse stderr: {}", e))?;
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    let stderr = String::from_utf8_lossy(&output.stderr).to_string();
 
     if !stderr.is_empty() {
         return Err(format!("stderr: {}", stderr));
@@ -31,6 +34,7 @@ fn run_script(script_path: &str) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, run_script])
